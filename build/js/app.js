4083da4c02f73421b7d14ff2d3174c86867773d8
Guest User <Guest@Epicodus-4B.local>
2018-02-08 15:16:30 -0800

    convert to last working version

--- build/js/app.js
+++ build/js/app.js
@@ -15,8 +15,8 @@ var Board = exports.Board = function () {
   }
 
   _createClass(Board, [{
-    key: 'checkIfboardValuesIsLegal',
-    value: function checkIfboardValuesIsLegal() {
+    key: 'checkIfBoardIsLegal',
+    value: function checkIfBoardIsLegal() {
       var gridValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
     }
   }, {
@@ -35,89 +35,26 @@ var Board = exports.Board = function () {
   }, {
     key: 'createGridValues',
     value: function createGridValues() {
-      var board = new Board();
-      var boardValuesValues = [];
+      var board = [];
       for (var row = 1; row <= 9; row++) {
-        var rowNumbers = board.getRandomNumbers();
-        boardValues.push([]);
+        var rowNumbers = getRandomNumbers();
+        board.push([]);
         for (var col = 1; col <= 9; col++) {
           var valueNotAdded = 0;
-          boardValues[row - 1].push(0);
+          board[row - 1].push(0);
           for (var index = 0; index < rowNumbers.length; index++) {
-            if (isSafe(boardValues, rowNumbers[index], row, col)) {
+            if (isSafe(board, rowNumbers[index], row, col)) {
               $("#" + row + "" + col).html('<p>' + rowNumbers[index] + '</p>');
-              boardValues[row - 1][col - 1] = parseInt(rowNumbers.splice(index, 1));
+              board[row - 1][col - 1] = parseInt(rowNumbers.splice(index, 1));
               index += 9;
             }
           }
         }
       }
 
-      for (var i = 0; i < boardValues.length; i++) {
-        console.log(boardValues[i]);
+      for (var i = 0; i < board.length; i++) {
+        console.log(board[i]);
       }
-    }
-  }, {
-    key: 'isSafe',
-    value: function isSafe(boardValues, number, numberRow, numberCol) {
-      var board = new Board();
-      var check = 0;
-      check += board.checkColumn(boardValues, number, numberCol);
-
-      check += board.checkSection(boardValues, number, numberRow, numberCol);
-
-      // console.log("Row Number: " + numberRow + " Check Number: " + check + " Number RC:" +numberRow+ "" +numberCol);
-      if (check > 0) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-  }, {
-    key: 'checkColumn',
-    value: function checkColumn(boardValues, number, numberCol) {
-      var output = 0;
-      for (var rows = 1; rows < boardValues.length; rows++) {
-        if (number == boardValues[rows - 1][numberCol - 1]) {
-          output += 1;
-        } else {}
-      }
-      // console.log("Column: " +numberCol+ " Number: " + number + " Output: " + output);
-      return output;
-    }
-  }, {
-    key: 'checkSection',
-    value: function checkSection(boardValues, number, numberRow, numberCol) {
-      var sRowMin = numberRow - 1 - (numberRow - 1) % 3;
-      var sColMin = numberCol - 1 - (numberCol - 1) % 3;
-      var sRowMax = sRowMin + 3;
-      var sColMax = sColMin + 3;
-      for (var rowI = sRowMin; rowI < boardValues.length; rowI++) {
-        for (var colI = sColMin; colI < boardValues[rowI].length; colI++) {
-          if (sRowMin <= rowI && rowI < sRowMax && sColMin <= colI && colI < sColMax) {
-            if (number == boardValues[rowI][colI]) {
-              return 1;
-            } else {}
-          }
-        }
-      }
-      return 0;
-    }
-  }, {
-    key: 'getRandomNumbers',
-    value: function getRandomNumbers() {
-      var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-      var currentIndex = array.length,
-          temporaryValue,
-          randomIndex;
-      while (0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-      }
-      return array;
     }
   }, {
     key: 'clearGrid',
@@ -128,6 +65,84 @@ var Board = exports.Board = function () {
 
   return Board;
 }();
+
+var isSafe = function isSafe(board, number, numberRow, numberCol) {
+  var check = 0;
+  check += checkColumn(board, number, numberCol);
+
+  // debugger;
+  // if(numberRow-1 < 3 && numberCol-1 < 3) {
+  //   check += checkSection(board,number,0,3,0,3);
+  // } else if(numberRow-1 < 3 && numberCol-1 < 6) {
+  //   check += checkSection(board,number,0,3,3,6);
+  // } else if(numberRow-1 < 3 && numberCol-1 < 9) {
+  //   check += checkSection(board,number,0,3,6,9);
+  // } else if(numberRow-1 < 6 && numberCol-1 < 3) {
+  //   check += checkSection(board,number,3,6,0,3);
+  // } else if(numberRow-1 < 6 && numberCol-1 < 6) {
+  //   check += checkSection(board,number,3,6,3,6);
+  // } else if(numberRow-1 < 6 && numberCol-1 < 9) {
+  //   check += checkSection(board,number,3,6,6,9);
+  // } else if(numberRow-1 < 9 && numberCol-1 < 3) {
+  //   check += checkSection(board,number,6,9,0,3);
+  // } else if(numberRow-1 < 9 && numberCol-1 < 6) {
+  //   check += checkSection(board,number,6,9,3,6);
+  // } else if(numberRow-1 < 9 && numberCol-1 < 9) {
+  //   check += checkSection(board,number,6,9,6,9);
+  // } else {
+  //   console.log("ERROR: Reached Elsed in Section IsSafe")
+  // }
+
+  check += checkSection(board, number, numberRow, numberCol);
+
+  // console.log("Row Number: " + numberRow + " Check Number: " + check + " Number RC:" +numberRow+ "" +numberCol);
+  if (check > 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
+var checkColumn = function checkColumn(board, number, numberCol) {
+  var output = 0;
+  for (var rows = 1; rows < board.length; rows++) {
+    if (number == board[rows - 1][numberCol - 1]) {
+      output += 1;
+    } else {}
+  }
+  // console.log("Column: " +numberCol+ " Number: " + number + " Output: " + output);
+  return output;
+};
+var checkSection = function checkSection(board, number, numberRow, numberCol) {
+  var sRowMin = numberRow - 1 - (numberRow - 1) % 3;
+  var sColMin = numberCol - 1 - (numberCol - 1) % 3;
+  var sRowMax = sRowMin + 3;
+  var sColMax = sColMin + 3;
+  for (var rowI = sRowMin; rowI < board.length; rowI++) {
+    for (var colI = sColMin; colI < board[rowI].length; colI++) {
+      if (sRowMin <= rowI && rowI < sRowMax && sColMin <= colI && colI < sColMax) {
+        if (number == board[rowI][colI]) {
+          return 1;
+        } else {}
+      }
+    }
+  }
+  return 0;
+};
+
+var getRandomNumbers = function getRandomNumbers() {
+  var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+};
 
 },{}],2:[function(require,module,exports){
 "use strict";
